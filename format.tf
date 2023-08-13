d8d7f068ac95cf47101865714daa128da439ea7a
Zabbeygroup <70457367+Zabbeygroup@users.noreply.github.com>
2023-08-13 08:03:32 -0400

    remove credentials

--- format.tf
+++ format.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAWVRIENYEL65QQEET"
-    secret_key = "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" 
+    access_key = ""
+    secret_key = "" 
 }
 
 resource "aws_instance" "ec2" {
